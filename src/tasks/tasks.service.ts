d9faa5fb0e93de2fe4f80e773abb1987b2ce3d4b
Karol Haniszewski <k.haniszewski@doittechnology.pl>
2021-02-22 12:16:49 +0100

    Getting & Updating a User's Task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -23,8 +23,14 @@ export class TasksService {
 	}
 
 
-	async getTaskById(id: number): Promise<Task> {
-		const found = await this.taskRepository.findOne(id);
+	async getTaskById(
+		id: number,
+		user: User
+	): Promise<Task> {
+		const found = await this.taskRepository.findOne(
+			{
+				where: { id, userId: user.id }
+			});
 
 		if (!found)
 			throw new NotFoundException(`Task with ID"${id}" not found`);
@@ -39,8 +45,12 @@ export class TasksService {
 		return this.taskRepository.createTask(createTaskDto, user);
 	}
 
-	async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-		const task = await this.getTaskById(id);
+	async updateTaskStatus(
+		id: number,
+		status: TaskStatus,
+		user: User
+	): Promise<Task> {
+		const task = await this.getTaskById(id, user);
 		task.status = status;
 
 		await task.save();
